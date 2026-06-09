a33f74fdc68586836f8d70801426c9d9a0be9434
EshanZaman <eshanzaman6699@gmail.com>
2026-06-09 23:35:33 +0600

    Refactor/Optimize dealer/map.php component

--- dealer/map.php
+++ dealer/map.php
@@ -1,1 +1,8 @@
-<?php require_once __DIR__.'/../customer/track.php';
+<?php
+session_start();
+require_once __DIR__.'/../includes/config.php';
+requireLogin(); requireRole('dealer');
+require_once __DIR__.'/../controllers/DealerController.php';
+
+$controller = new DealerController(getDB());
+$controller->map();
