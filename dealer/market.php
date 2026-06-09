93266312ced3608a8ce0a251c0f991461a6d2b28
EshanZaman <eshanzaman6699@gmail.com>
2026-06-09 23:35:36 +0600

    Refactor/Optimize dealer/market.php component

--- dealer/market.php
+++ dealer/market.php
@@ -1,1 +1,8 @@
-<?php require_once __DIR__.'/../supplier/market.php';
+<?php
+session_start();
+require_once __DIR__.'/../includes/config.php';
+requireLogin(); requireRole(['supplier','dealer']);
+require_once __DIR__.'/../controllers/DealerController.php';
+
+$controller = new DealerController(getDB());
+$controller->market();
