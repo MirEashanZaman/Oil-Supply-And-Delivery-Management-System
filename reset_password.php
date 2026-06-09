8f0be1e286b778afbab332610d4a8d9a494bc1f3
EshanZaman <eshanzaman6699@gmail.com>
2026-06-09 23:48:22 +0600

    Feature: Add reset_password.php component for mapping and reset flows

---
+++ reset_password.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/controllers/AuthController.php';
+
+$controller = new AuthController(getDB());
+$controller->resetPassword();
